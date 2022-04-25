ea8b1165ea5202a9877238e44b3c56f7ea22269c
mavolle <florian.mavolle@gmail.com>
2022-04-25 22:39:23 +0200

    Import data clean

--- Partie 1.py
+++ Partie 1.py
@@ -4,44 +4,37 @@ from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster, cut_tree
 from matplotlib import pyplot as plt
 
-df = pd.read_csv("Subject/DATA_PROJECT.csv", index_col=0, sep=';')
-
-for col in df.columns[0:]:
-    df[col]=df[col].apply(lambda x:float(x))
-
+df = pd.read_csv("Data/DATA_PROJECT.csv", index_col=0)
 corrMatrix = df.corr()
 
-returns=df.drop(df.columns[-1], axis=1)
-label=returns.columns
-returns=preprocessing.scale(returns)
+returns = df.drop(df.columns[-1], axis=1)
+label = returns.columns
+returns = preprocessing.scale(returns)
 
-dist=pdist(returns.T,'correlation')
-output = linkage(dist,method='ward')
-
+dist = pdist(returns.T, 'correlation')
+output = linkage(dist, method='ward')
 
 plt.figure(figsize=(10, 10))
-csfont = {'fontname':'Calibri', 'fontsize' : '10'}
-csfont2 = {'fontname':'Calibri', 'fontsize' : '8'}
-csfont3 = {'fontname':'Calibri', 'fontsize' : '6'}
-plt.xlabel('Distance',**csfont2)
+csfont = {'fontname': 'Calibri', 'fontsize': '10'}
+csfont2 = {'fontname': 'Calibri', 'fontsize': '8'}
+csfont3 = {'fontname': 'Calibri', 'fontsize': '6'}
+plt.xlabel('Distance', **csfont2)
 plt.xticks(**csfont3)
 plt.yticks(**csfont3)
-dendrogram(output,color_threshold=1.5,truncate_mode='level',orientation='right',leaf_font_size=10,labels=label)
-plt.savefig('books_read.jpeg',bbox_inches='tight', dpi=300)
+dendrogram(output, color_threshold=1.5, truncate_mode='level', orientation='right', leaf_font_size=10, labels=label)
+plt.savefig('Data/books_read.jpeg', bbox_inches='tight', dpi=300)
 plt.show()
 
+clusters = fcluster(output, t=1.3, criterion='distance')
 
-clusters=fcluster(output,t=1.3,criterion='distance')
-
-cluster_1=[]
-cluster_2=[]
+cluster_1 = []
+cluster_2 = []
 
 for i in range(len(clusters)):
-    if clusters[i]==1:
+    if clusters[i] == 1:
         cluster_1.append(label[i])
     else:
         cluster_2.append(label[i])
 
-df1=pd.DataFrame(index=label)
-df1['clusters']=clusters
-
+df1 = pd.DataFrame(index=label)
+df1['clusters'] = clusters
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 from sklearn import preprocessing
 from scipy.spatial.distance import pdist, squareform
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster, cut_tree
+from matplotlib import pyplot as plt
 
-
-df = pd.read_csv("Subject/DATA_PROJECT.csv", index_col=0, sep=';')
+df = pd.read_csv("Data/DATA_PROJECT.csv", index_col=0, sep=';')
 
 corrMatrix = df.corr()
 print(corrMatrix)
-
 
 # 1. Hierarchical clustering
 # ✓ PCA decomposition of the covariance matrix. ✓ Features = ARP exposures to PCs
@@ -15,27 +15,23 @@ print(corrMatrix)
 # - Ward linkage
 # - Euclidean distance
 
-# pca = PCA()
-# x_train = pca.fit_transform(x_train)
-# x_val = pca.transform(x_val)
-# explained_variance = pca.explained_variance_ratio_
-# res_PCA = pd.DataFrame({'Expl. variance' : np.round(explained_variance,2)}, index = ['PCA' + str(i) for i in range(9)])
-# res_PCA
-# plt.plot(res_PCA)
+# pca = PCA() x_train = pca.fit_transform(x_train) x_val = pca.transform(x_val) explained_variance =
+# pca.explained_variance_ratio_ res_PCA = pd.DataFrame({'Expl. variance' : np.round(explained_variance,2)},
+# index = ['PCA' + str(i) for i in range(9)]) res_PCA plt.plot(res_PCA)
 
-returns=preprocessing.scale(df)
+returns = preprocessing.scale(df)
 
-dist=pdist(returns.T,'correlation')
-output = linkage(dist,method='ward')
+dist = pdist(returns.T, 'correlation')
+output = linkage(dist, method='ward')
 
 # GRAPH
 plt.figure(figsize=(10, 10))
-csfont = {'fontname':'Calibri', 'fontsize' : '10'}
-csfont2 = {'fontname':'Calibri', 'fontsize' : '8'}
-csfont3 = {'fontname':'Calibri', 'fontsize' : '6'}
-plt.xlabel('Distance',**csfont2)
+csfont = {'fontname': 'Calibri', 'fontsize': '10'}
+csfont2 = {'fontname': 'Calibri', 'fontsize': '8'}
+csfont3 = {'fontname': 'Calibri', 'fontsize': '6'}
+plt.xlabel('Distance', **csfont2)
 plt.xticks(**csfont3)
 plt.yticks(**csfont3)
-dendrogram(output,color_threshold=1.5,truncate_mode='level',orientation='right',leaf_font_size=10,labels=LABl)
-plt.savefig('books_read.jpeg',bbox_inches='tight', dpi=300)
+dendrogram(output, color_threshold=1.5, truncate_mode='level', orientation='right', leaf_font_size=10, labels=LABl)
+plt.savefig('books_read.jpeg', bbox_inches='tight', dpi=300)
 plt.show()
