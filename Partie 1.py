5edce35bc0b758f63c853004b7eda14f67d888f3
mavolle <florian.mavolle@gmail.com>
2022-04-26 11:09:28 +0200

    rolling

--- Partie 1.py
+++ Partie 1.py
@@ -22,19 +22,23 @@ plt.xlabel('Distance', **csfont2)
 plt.xticks(**csfont3)
 plt.yticks(**csfont3)
 dendrogram(output, color_threshold=1.5, truncate_mode='level', orientation='right', leaf_font_size=10, labels=label)
-plt.savefig('Data/books_read.jpeg', bbox_inches='tight', dpi=300)
-plt.show()
+#plt.savefig('Data/books_read.jpeg', bbox_inches='tight', dpi=300)
+#plt.show()
 
 clusters = fcluster(output, t=1.3, criterion='distance')
+df_clusters = pd.DataFrame(index=label)
+df_clusters['clusters'] = clusters
 
-cluster_1 = []
-cluster_2 = []
+df_clusters_1 = df[df_clusters[df_clusters['clusters'] == 1].index.values]
+df_clusters_2 = df[df_clusters[df_clusters['clusters'] == 2].index.values]
 
-for i in range(len(clusters)):
-    if clusters[i] == 1:
-        cluster_1.append(label[i])
-    else:
-        cluster_2.append(label[i])
+def twelve_month_momentum(df):
+    df_output = df.rolling(2).apply(lambda x: x.mean())
+    return df_output
 
-df1 = pd.DataFrame(index=label)
-df1['clusters'] = clusters
+print(df_clusters_1.head(10))
+print(twelve_month_momentum(df_clusters_1.head(10)))
+# Momentum
+
+
+
